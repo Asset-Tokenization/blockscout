9ced60111d69b95448953083cc5d1c2150c6128c
Huy Lam <lnhuy1912@gmail.com>
2023-07-04 22:33:33 +0700

    MPT-4 | Custom Side Explorer

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -10,26 +10,31 @@ defmodule BlockScoutWeb.LayoutView do
   @issue_url "https://github.com/blockscout/blockscout/issues/new"
   @default_other_networks [
     %{
-      title: "POA",
-      url: "https://blockscout.com/poa/core"
+      title: "BNB Chain",
+      url: "https://bscscan.com/"
     },
     %{
-      title: "Sokol",
-      url: "https://blockscout.com/poa/sokol",
+      title: "Ethereum",
+      url: "https://etherscan.io/"
+    },
+    %{
+      title: "BNB Testnet",
+      url: "https://testnet.bscscan.com/",
       test_net?: true
     },
     %{
-      title: "Gnosis Chain",
-      url: "https://blockscout.com/xdai/mainnet"
+      title: "Goerli",
+      url: "https://goerli.etherscan.io/",
+      test_net?: true
     },
     %{
-      title: "Ethereum Classic",
-      url: "https://blockscout.com/etc/mainnet",
+      title: "Polygon",
+      url: "https://polygonscan.com/",
       other?: true
     },
     %{
-      title: "RSK",
-      url: "https://blockscout.com/rsk/mainnet",
+      title: "Avalanche",
+      url: "https://avascan.info/",
       other?: true
     }
   ]
